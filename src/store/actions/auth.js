f21c37e0f3c5e539bd56a759998d15789306f5f8
vasy <kitten20811@gmail.com>
2020-03-23 21:41:58 +0200

    quiz with redux

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -1,43 +1,69 @@
 import axios from 'axios';
-import {AUTH_SUCCESS, AUTH_LOGOUT} from './actionTypes'
+import { AUTH_SUCCESS, AUTH_LOGOUT } from './actionTypes'
 
 export const auth = (email, password, isLogin) => async dispatch => {
 
-    const authData = {
-      email,
-      password,
-      returnSecureToken: true
+  const authData = {
+    email,
+    password,
+    returnSecureToken: true
+  }
+
+  let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyASPf2PErnL4fAEah-SlEOehHupIchB_kE'
+
+  if (isLogin) {
+    url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyASPf2PErnL4fAEah-SlEOehHupIchB_kE'
+  }
+  const response = await axios.post(url, authData)
+  const data = response.data
+
+
+
+  const expirationDate = new Date(new Date().getTime() + data.expiresIn * 1000)
+
+  localStorage.setItem('token', data.idToken)
+  localStorage.setItem('userId', data.localId)
+  localStorage.setItem('expirationDate', expirationDate)
+  dispatch(authSuccess(data.idToken))
+  dispatch(autoLogout(data.expiresIn))
+  }
+
+
+
+export const autoLogout = (time) => dispatch => {
+  
+    setTimeout(() => {
+      dispatch(logout())
+      
+    }, time * 1000/60)
+  }
+
+
+export const autoLogin = () => dispatch => {
+
+  const token = localStorage.getItem('token')
+  if (!token) {
+    dispatch(logout())
+  } else {
+    const expirationDate = new Date(localStorage.getItem('expirationDate'))
+    if (expirationDate <= new Date()) {
+      dispatch(logout())
+    } else {
+      dispatch(authSuccess(token))
+      dispatch(autoLogout((expirationDate.getTime() - new Date().getTime()) / 1000))
     }
-
-    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyASPf2PErnL4fAEah-SlEOehHupIchB_kE'
-    
-    if(isLogin){
-        url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyASPf2PErnL4fAEah-SlEOehHupIchB_kE'
-    }
-    const response = await axios.post(url, authData)
-    const data = response.data
-    
-    const exparetionDate = new Date(new Date().getTime() + data.expiresIn*1000)
-
-      localStorage.setItem('token', data.idToken)
-      localStorage.setItem('userId', data.localId)
-      localStorage.setItem('exparetionDate', exparetionDate)
-    dispatch(authSuccess(data.idToken))
-    dispatch(autoLOgout(data.expiresIn))
-
-    
+  }
 }
 
-export const autoLOgout = (time) => dispatch => {
-    setTimeout(() => {
-        dispatch(logout())
-      localStorage.removeItem('token')
-      localStorage.removeItem('userId')
-      localStorage.removeItem('exparetionDate')
-    }, time * 1000)
-} 
 
+export const authSuccess = (token) => ({ type: AUTH_SUCCESS, token })
 
-export const authSuccess = (token) => ({type: AUTH_SUCCESS, token})
-export const logout = () => ({type: AUTH_LOGOUT})
-    
+export const logout = () => {
+  localStorage.removeItem('token')
+  localStorage.removeItem('userId')
+  localStorage.removeItem('expirationDate')
+  return {
+    type: AUTH_LOGOUT
+  }
+}
+
